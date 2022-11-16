eafad17e258874a1eff777e39568b1db99eb63d0
Sehan Arandara <99310968+SehanArandara@users.noreply.github.com>
2022-11-17 04:34:18 +0530

    Thread implements Runnable

---
+++ Thread03.java
@@ -0,0 +1,64 @@
+package thread;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		//to start the child thread we have to create a thread object
+		thread01 t01 = new thread01();
+		// to execute the run() method we have to call the start method
+		t01.start();
+		
+		// object creating and parsing of objects in Runnable interface
+		thread02 td = new thread02();
+		Thread t02 = new Thread(td);
+		t02.start();
+		
+		// main thread
+		for(int i=1;i<=10;i++) {
+			System.out.println("Main Thread "+i);
+			try {   //thread.sleep create an unchecked exception so that we have to solve the problem to run the program
+				Thread.sleep(100);    // do the first iteration and sleep for 100 miliseconds
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+// thread 01  class that extends inbuilt thread class
+
+class thread01 extends Thread{
+	
+	// we have to override this run() method
+	public void run() {
+		for(int i=1;i<=10;i++) {
+			System.out.println("child thread "+i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
+
+class thread02 implements Runnable{
+	// we have to override this run() method
+		public void run() {
+			for(int i=1;i<=10;i++) {
+				System.out.println("child thread02 "+i);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+}
